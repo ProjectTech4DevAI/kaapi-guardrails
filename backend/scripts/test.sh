70dcfea75cfd78ab0d620100bd489ef23e9a9d6c
rkritika1508 <rkritika1508@gmail.com>
2026-01-28 13:15:53 +0530

    Revert test.sh to main

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-coverage run -m pytest app/tests -m "not slow and not redteam"
+coverage run -m pytest app/tests
 coverage report --show-missing
 coverage html --title "${@-coverage}"
 coverage xml
